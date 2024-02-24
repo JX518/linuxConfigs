35c639c669c850498a7cfc09c479b172e0f90625
Justin Xu <xujustin7@gmail.com>
2024-02-24 11:15:55 -0700

    modified:   copyToHere.sh 	new file:   scripts/swww1.sh

--- copyToHere.sh
+++ copyToHere.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp -r /home/xujus/.config/hypr/hyprland.conf /home/xujus/.config/waybar/* /home/xujus/.config/brave-flags.conf /home/xujus/Documents/configsGit/configs/
-cp -r /home/xujus/Documents/scripts/ /home/xujus/Documents/configsGit/scripts/
+cp -r /home/xujus/Documents/scripts/* /home/xujus/Documents/configsGit/scripts/
  
