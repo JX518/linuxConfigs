6cc2c03e36c01ae0f35e441353ebdcf40b968350
Justin Xu <xujustin7@gmail.com>
2024-02-23 17:02:22 -0700

    new file:   README.mdn 	modified:   gitpush.sh 	deleted:    "README.md\n" 	deleted:    README.mdn

--- gitpush.sh
+++ gitpush.sh
@@ -5,5 +5,7 @@ ssh-add ~/.ssh/github
 ssh -T git@github.com
 git remote add configs git@github.com:JX518/newRepo.git
 git branch -M main
+git add /home/xujus/Documents/configsGit/*
+
 git commit
 git push -u configs main
