201a0e82dc707dca48dfbae63a98f9bdb9ac20f0
Justin Xu <xujustin7@gmail.com>
2024-02-28 17:23:56 -0700

    modified:   configs/.alacritty.toml 	modified:   configs/hyprland.conf 	modified:   configs/style.css 	modified:   scripts/changeViews.sh

--- scripts/changeViews.sh
+++ scripts/changeViews.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if cmp /home/xujus/.config/hypr/2.conf /home/xujus/.config/hypr/hyprland.conf
 then
-	cp /home/xujus/.config/hypr/1.conf /home/xujus/.config/hypr/hyprland.conf 
+	cp /home/xujus/.config/hypr/1.conf /home/xujus/.config/hypr/hyprland.conf
 else
 	cp /home/xujus/.config/hypr/2.conf /home/xujus/.config/hypr/hyprland.conf
 fi
