208b7b34ccb4474c01c610528b403a5023394537
Justin Xu <xujustin7@gmail.com>
2024-08-13 07:59:26 -0700

    finished the light and dark style

--- copyToHere.sh
+++ copyToHere.sh
@@ -8,17 +8,21 @@ cp /bin/hyprshot-gui /home/xujus/Documents/configsGit/scripts/hyprshot-gui
 
 #hyprland
 cp -r /home/xujus/.config/hypr/hyprland.conf /home/xujus/Documents/configsGit/configs/hyprland.conf
+cp -r /home/xujus/.config/hypr/hyprland.conf /home/xujus/Documents/configsGit/configs/hyprlandLight.conf
+cp -r /home/xujus/.config/hypr/hyprland.conf /home/xujus/Documents/configsGit/configs/hyprlandDark.conf
 cp -r /home/xujus/.config/hypr/hyprpaper.conf /home/xujus/Documents/configsGit/configs/hyprpaper.conf
+
+#waybar
+cp -r /home/xujus/.config/waybar/style.css /home/xujus/Documents/configsGit/configs/style.css
+cp -r /home/xujus/.config/waybar/style.css /home/xujus/Documents/configsGit/configs/styleLight.css
+cp -r /home/xujus/.config/waybar/style.css /home/xujus/Documents/configsGit/configs/styleDark.css
+cp -r /home/xujus/.config/waybar/config /home/xujus/Documents/configsGit/configs/config
 
 #linux environment
 cp -r /etc/environment /home/xujus/Documents/configsGit/configs/environment
 
 #emacs
 cp -r /home/xujus/.emacs /home/xujus/Documents/configsGit/configs/.emacs
-
-#waybar
-cp -r /home/xujus/.config/waybar/style.css /home/xujus/Documents/configsGit/configs/style.css
-cp -r /home/xujus/.config/waybar/config /home/xujus/Documents/configsGit/configs/config
 
 #brave
 cp -r /home/xujus/.config/brave-flags.conf /home/xujus/Documents/configsGit/configs/brave-flags.conf
