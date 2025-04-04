86c44e1a84c073b797f9b19d3c6327fb8271a969
Justin Xu <xujustin7@gmail.com>
2025-04-04 16:52:26 -0700

    modified

--- gitpush.sh
+++ gitpush.sh
@@ -3,8 +3,8 @@ cd /home/xujus/Documents/configsGit
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/github
 ssh -T git@github.com
-git remote add configs git@github.com:JX518/newRepo.git
+git remote add configs git@github.com:JX518/linuxConfigs.git
 git branch -M main
 git add /home/xujus/Documents/configsGit/*
 git commit
-git push -u configs main
+git push -u origin main
